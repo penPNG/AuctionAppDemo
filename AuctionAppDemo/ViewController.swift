fd424f47799773ca6f52ea3da38e9c0a23fed1b7
penPNG <graeme.roach01@yahoo.com>
2025-05-21 13:11:19 -0400

    Initial Commit

---
+++ AuctionAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuctionAppDemo
+//
+//  Created by student on 5/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AuctionAppDemoTests/AuctionAppDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuctionAppDemoTests.swift
+//  AuctionAppDemoTests
+//
+//  Created by student on 5/21/25.
+//
+
+import Testing
+@testable import AuctionAppDemo
+
+struct AuctionAppDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
