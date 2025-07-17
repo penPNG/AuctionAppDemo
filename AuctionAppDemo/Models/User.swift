74a68a60134f6580b740fc8c25661dbe9505597e
penPNG <graeme.roach01@yahoo.com>
2025-07-16 21:18:27 -0400

    Create functions for validating users

--- AuctionAppDemo/Models/User.swift
+++ AuctionAppDemo/Models/User.swift
@@ -43,3 +43,23 @@ func emptyUser() -> User {
                 company: Company(bs: nil, catchPhrase: nil, name: nil),
                 email: nil, id: 0, name: nil, phone: nil, username: nil, website: nil)
 }
+
+func validateEmail(with email: String) -> Bool {
+    let emailRegex = "\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\b"
+    let emailPred = NSPredicate(format:"SELF MATCHES[c] %@", emailRegex)
+    
+    return emailPred.evaluate(with: email)
+}
+
+// This can be much further expanded
+func validateUser(with user: User, completion: @escaping (Bool, String, String) -> Void) {
+    if user.name == nil || user.name!.isEmpty {
+        completion(false, "Invalid Name", "Name cannot be empty")
+    } else if user.username == nil || user.username!.isEmpty {
+        completion(false, "Invalid Username", "Username cannot be empty")
+    } else if validateEmail(with: user.email ?? "") == false {
+        completion(false, "Invalid Email", "Please check email address")
+    } else {
+        completion(true, "Please report this", "This is a bug")
+    }
+}
